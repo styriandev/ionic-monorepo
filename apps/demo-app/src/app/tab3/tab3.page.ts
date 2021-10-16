e86d65b9db72fc7c89e3f734daff33d70ee19cfd
styriandev <dominiksammer@inode.at>
2021-10-16 14:14:14 +0200

    Initial commit

---
+++ apps/demo-app/src/app/tab3/tab3.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ionic-workspace-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+})
+export class Tab3Page {
+  constructor() {}
+}
---
+++ apps/demo-app/src/app/tabs/tabs.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ionic-workspace-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+})
+export class TabsPage {
+  constructor() {}
+}
